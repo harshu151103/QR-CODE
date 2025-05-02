89bb991ab6a7fb89364e81ed5eaff17b571bf350
Harsh Upreti <98612598+harshu151103@users.noreply.github.com>
2025-05-02 17:39:46 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   const [qr, setQr] = useState('');
 
   useEffect(() => {
-    // QRCode.toDataURL('https://qr-code-lime-one.vercel.app/letstry')
+    QRCode.toDataURL('https://qr-code-lime-one.vercel.app/letstry')
       // QRCode.toDataURL('https://letstry-qr.com/letstry')
       // QRCode.toDataURL('https://www.letstryfoods.com/?srsltid=AfmBOor9Ee6fF3tAdVonprruJ6wc-vvsCanXloiYqeNJU3qT5gYW1Tjj')
       QRCode.toDataURL('https://www.letstryfoods.com')
